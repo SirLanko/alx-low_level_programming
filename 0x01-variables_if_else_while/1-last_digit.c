ac069caa697968e08f7b72f6b4aa1782d5744612
SirLanko <afeez_salami@yahoo.com>
2022-03-11 18:03:02 +0100

    Adding 1-last_digit.c, and Betty did not have any complain

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,33 +1,30 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
 /**
- * main - print if number is positive, zero, or negative
+ * main - Determines either greater than 5, is less than 6, or is 0
  *
- * Description: using the main function
- * this program prints "prints the last digit store in n
- * Return: 0
+ * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
-int n;
-int l;
+	int n, l;
 
-srand(time(0));
-n = rand() - RAND_MAX / 2
-l = l % 10
-
-if (1 > 5)
-{
-	printf("Last digit of %d is %d and is greater than 5\n", n, l);
-}
-else if (1 == 0)
-{
-	printf("last digit of %d is %d and is 0\n", n. l);
-}
-else
-{
-	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
-}
-return (0);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	l = n % 10;
+	if (l > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, l);
+	}
+	else if (l == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, l);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
+	}
+	return (0);
 }
