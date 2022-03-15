650c11a3cec2f324a544eb559de599c7caf7383a
SirLanko <afeez_salami@yahoo.com>
2022-03-15 17:59:07 +0100

    C-functions, nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * main - function to print
+ * main - function to print holberton message
  *
- * Return: 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char _putchar[9] = {'H', 'o', 'l', 'b', 'e', 'r', 't', 'o', 'n'};
+	char hoberton[9] = {'H', 'o', 'l', 'b', 'e', 'r', 't', 'o', 'n'};
 	unsigned int c;
 
 	for (c = 0; c < sizeof(holberton); c++)
